4c0d0e010538aba37604b26b3b194447d0a8a86f
umakundla <114097157+umakundla@users.noreply.github.com>
2022-09-21 16:48:54 +0000

    Create instance.tf

---
+++ compute/instance.tf
@@ -0,0 +1,7 @@
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/umakundla/Terraform-oci.git
+git push -u origin mainecho "# Terraform-oci" >> README.md
